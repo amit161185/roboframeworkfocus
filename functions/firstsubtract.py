045f9d2d609fc4acbb0265b629c4c4c4333586ed
amit161185 <moris@354>
2019-02-27 18:40:09 +0530

    sum,subtract function added

---
+++ functions/firstsubtract.py
@@ -0,0 +1,14 @@
+class FirstSubtract:
+
+    def __init__(self,no3,no4):
+        print("you are in subtract class@@")
+
+    def sub(self,a,b):
+        if (a>b):
+            minus =a-b
+            print("value after sbutract",minus)
+        elif(a < b):
+            print("a is small than b")
+
+
+print("hello subtract class" , __name__)
